bb81749b5f5ff8e16b0778c2f166b69c2e13197d
Felicia Kwame <kwamefelicia2005@gmail.com>
2024-09-01 14:10:03 +0300

    final commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    
+    if length <= 0:
+        print([])
+        return
+    
+    fibonacci_sequence = []
+    
+    if length >= 1:
+        fibonacci_sequence.append(0)
+    if length >= 2:
+        fibonacci_sequence.append(1)
+    
+    while len(fibonacci_sequence) < length:
+        next_value = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_value)
+    
+    print(fibonacci_sequence)
+
+
+print_fibonacci(9)
+
